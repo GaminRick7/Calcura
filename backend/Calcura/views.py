6c4d8f1dd1bc24ef1c5cf7979a3964d66063548c
Iwillgetmy <dkanesal3@gmail.com>
2023-02-07 09:20:13 -0500

    editListing

--- backend/Calcura/views.py
+++ backend/Calcura/views.py
@@ -6,7 +6,6 @@ from django.contrib import messages
 from .models import Calculator, TempImage
 from django.contrib.auth.decorators import login_required
 
-imgursecret="effc638490e3951b29079d257969bdf93e4bb773"
 
 # The view to handle the home page
 def Index(request):
@@ -90,3 +89,53 @@ def createListing(request):
         a=Calculator(title=title, description=description,image=imageUrls,price=price,tags=tags,email=request.user.email)
         a.save()
     return render(request, "calcura/createListing.html")
+
+def editListing(request, id):
+    listing=Calculator
+    if id>len(Calculator.objects.all()):
+        return HttpResponseRedirect("/")
+    for c in Calculator.objects.all():
+        if c.id==id:
+            if c.email==request.user.email:
+                listing=c
+            else:
+                return HttpResponseRedirect("/vendorPage")
+    if request.method=="POST":
+        title=request.POST['title']
+        price=float(request.POST['price'])
+        description=request.POST['description']
+        tags=request.POST['tags']
+        images=request.FILES.getlist('docfile')
+        #Round the price to two decimal places
+        price = round(float(price),2)
+
+        #Looping through the images they pasted, and storing them in TempImage database model. Storing them to upload to cloudinary and to get image link url. Email is needed to link a temporary image to the user
+        for image in images:
+            listing = TempImage(image= image, email=request.user.email)
+            listing.save()
+
+        #Getting the images stored in TempImage, and defining a string to store the urls of images
+        images=TempImage.objects.all()
+        imageUrls=""
+
+        #Go through the images, and if the image belongs to the user, add it to the imageUrls string. After that, delete the image from the database for storage purposes
+        for image in images:
+            if image.email==request.user.email:
+                imageUrls += str(image.image.url) + ","
+                image.delete()
+        print(imageUrls)
+
+        listing.title=title
+        listing.price=price
+        listing.description=description
+        listing.tags=tags
+        listing.image=imageUrls
+        print(listing.title)
+        print(listing.price)
+        print(listing.description)
+        print(listing.tags)
+        print(listing.image)
+        Calculator.objects.filter(id=id).update(title=title,price=price,description=description,tags=tags,image=imageUrls)
+        
+
+    return render(request, "calcura/editListing.html", {"l": listing})
