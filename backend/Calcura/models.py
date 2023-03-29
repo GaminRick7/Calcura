728d4f2bcca8180eb251c31ffe7a8bd7e604d2b5
Raihaan Sandhu <63803872+GaminRick7@users.noreply.github.com>
2023-03-29 16:06:26 -0400

    Admin Changes

--- backend/Calcura/models.py
+++ backend/Calcura/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from chatSystem.models import Messages
 import datetime
 
 # Create your models here.
@@ -21,15 +22,15 @@ class Calculator(models.Model):
     image = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=1000)
     tags = models.TextField()
-    email = models.TextField()
-    fullname = models.TextField()
+    # email = models.TextField()
+    # fullname = models.TextField()
     datetime = models.DateTimeField(default=datetime.datetime.now())
     id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
 
     def __str__(self):
-        correctUser = User.objects.get(email=self.email)
-        return f"{self.title} by {correctUser.get_full_name()}"
+        return f"{self.title} by {self.user.get_full_name()}"
     
 
 class TempImage(models.Model):
@@ -58,4 +59,10 @@ class MessageRoom(models.Model):
         id (int): id of the group (created by defalt in django)
     """
     users=models.TextField()
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first().pk, related_name="user1")
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first().pk)
     id = models.BigAutoField(primary_key=True)
+    messages = models.ManyToManyField(Messages)
+
+    def __str__(self):
+        return f"{self.user1.get_full_name()} and {self.user2.get_full_name()}"
